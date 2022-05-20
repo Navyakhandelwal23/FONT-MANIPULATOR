4d4da23c662502de42f9bbfe8c5f54a98e9daf85
Navyakhandelwal23 <83491933+Navyakhandelwal23@users.noreply.github.com>
2022-05-20 14:37:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+nosex= 0
+nosey= 0
+difference= 0
+leftwristx= 0
+rightwristx= 0
+
+function setup(){
+    canvas= createCanvas(550, 550)
+    canvas.position(560,150)
+    video= createCapture(VIDEO)
+    video.size(550,500)
+    posenet= ml5.poseNet(video, modelloaded)
+    posenet.on('pose', gotposes)
+}
+
+function modelloaded(){
+    console.log("model is loaded")
+}
+
+function gotposes(results){
+    if(results.length>0){
+        console.log(results)
+        nosex= results[0].pose.nose.x
+        nosey= results[0].pose.nose.y
+        leftwristx= results[0].pose.leftWrist.x
+        rightwristx= results[0].pose.rightWrist.x
+        difference= floor(leftwristx-rightwristx)
+    }
+}
+
+function draw(){
+    background("cyan")
+    document.getElementById("square_side").innerHTML= "width and hieght of the square is "+difference+"px"
+    fill("pink")
+    stroke("pink")
+    square(nosex,nosey,difference)
+}
+
+
+
